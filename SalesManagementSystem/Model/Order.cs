8c385b5d6befb0c47dea1145ccaeae011492e865
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-07-29 12:20:31 +0100

    updated

--- SalesManagementSystem/Model/Order.cs
+++ SalesManagementSystem/Model/Order.cs
@@ -6,7 +6,7 @@
         public Guid CustomerId { get; set; }
         public Guid GoodId { get; set; } 
         public int Quantity { get; set; }
-        public OrderStatus OrderStatus { get; set; } = OrderStatus.Pending;
+        public OrderStatus OrderStatus { get; set; } = OrderStatus.NotPaid;
         public float Amount { get; set; }
     }
 }
