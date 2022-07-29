011baba36240b37e7b776bae4e7a4b19b199e32c
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-07-29 10:51:36 +0100

    added payment model

---
+++ SalesManagementSystem/Model/Payment.cs
@@ -0,0 +1,16 @@
+﻿namespace SalesManagementSystem.Model
+{
+    public class Payment
+    {
+      
+        public Payment()
+        {
+            PaymentId = Guid.NewGuid();
+        }
+        public Guid PaymentId { get; set; }
+        public Guid OrderId { get; set; }
+        public float Amount { get; set; }
+        public float Balance { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
